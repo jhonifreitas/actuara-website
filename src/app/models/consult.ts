85ad2a129c8c8e505abad7eac5fe0cbd0d0ad1ed
jhonifreitas <freitas.jonathan08@gmail.com>
2021-06-15 19:22:04 -0300

    consult

--- src/app/models/consult.ts
+++ src/app/models/consult.ts
@@ -4,9 +4,11 @@ import { Company as HubDev } from '../interfaces/hub-dev';
 export class Consult extends Base {
   company: Company;
   result!: HubDev;
+  quantity: number;
 
   constructor() {
     super();
+    this.quantity = 1;
     this.company = new Company();
   }
 }
